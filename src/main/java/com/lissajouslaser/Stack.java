b3873dd7c6495df443b02d4b13e2c178aae15e00
lissajous-laser <wooden_box@hotmail.com>
2022-07-08 22:25:05 +1000

    initial commit

---
+++ src/main/java/com/lissajouslaser/Stack.java
@@ -0,0 +1,14 @@
+package com.lissajouslaser;
+
+
+// encapsulates stack used to store values
+public class Stack {
+   private int[] numbers;
+
+   Stack() {
+      numbers = new int[4];
+   } 
+
+
+
+}
